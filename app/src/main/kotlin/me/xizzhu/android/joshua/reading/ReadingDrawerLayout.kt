044e93c8fd4ffd2f1e7929292673dc26d51d4e76
Xizhi Zhu <xizhi.zhu@gmail.com>
2019-02-27 23:08:38 -0800

    Made reading activity simpler

---
+++ app/src/main/kotlin/me/xizzhu/android/joshua/reading/ReadingDrawerLayout.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 2019 Xizhi Zhu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.xizzhu.android.joshua.reading
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.filter
+import kotlinx.coroutines.launch
+import me.xizzhu.android.joshua.utils.MVPPresenter
+import me.xizzhu.android.joshua.utils.MVPView
+import me.xizzhu.android.joshua.utils.onEach
+
+class ReadingDrawerPresenter(private val readingViewController: ReadingViewController) : MVPPresenter<ReadingDrawerView>() {
+    override fun onViewAttached() {
+        super.onViewAttached()
+
+        launch(Dispatchers.Main) {
+            receiveChannels.add(readingViewController.observeCurrentVerseIndex()
+                    .filter { it.isValid() }
+                    .onEach { view?.hide() })
+        }
+    }
+}
+
+interface ReadingDrawerView : MVPView {
+    fun hide()
+}
+
+class ReadingDrawerLayout : DrawerLayout, ReadingDrawerView {
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    override fun hide() {
+        closeDrawer(GravityCompat.START)
+    }
+}
---
+++ app/src/main/kotlin/me/xizzhu/android/joshua/reading/SearchFAB.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2019 Xizhi Zhu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.xizzhu.android.joshua.reading
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import me.xizzhu.android.joshua.utils.MVPPresenter
+import me.xizzhu.android.joshua.utils.MVPView
+
+class SearchButtonPresenter(private val readingViewController: ReadingViewController) : MVPPresenter<SearchButtonView>() {
+    fun openSearch() {
+        readingViewController.openSearch()
+    }
+}
+
+interface SearchButtonView : MVPView
+
+class SearchFloatingActionButton : FloatingActionButton, SearchButtonView, View.OnClickListener {
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    init {
+        setOnClickListener(this)
+    }
+
+    private lateinit var presenter: SearchButtonPresenter
+
+    fun setPresenter(presenter: SearchButtonPresenter) {
+        this.presenter = presenter
+    }
+
+    override fun onClick(v: View) {
+        presenter.openSearch()
+    }
+}
