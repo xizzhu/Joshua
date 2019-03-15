1a94f1bb1940b52b16ebaec37270fc742c9d4896
Xizhi Zhu <xizhi.zhu@gmail.com>
2019-03-15 15:55:43 -0700

    Added reading progress table

--- app/src/main/kotlin/me/xizzhu/android/joshua/core/repository/local/android/AndroidDatabase.kt
+++ app/src/main/kotlin/me/xizzhu/android/joshua/core/repository/local/android/AndroidDatabase.kt
@@ -22,10 +22,7 @@ import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import androidx.annotation.WorkerThread
-import me.xizzhu.android.joshua.core.Bible
-import me.xizzhu.android.joshua.core.TranslationInfo
-import me.xizzhu.android.joshua.core.Verse
-import me.xizzhu.android.joshua.core.VerseIndex
+import me.xizzhu.android.joshua.core.*
 import java.lang.StringBuilder
 
 class AndroidDatabase(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
@@ -36,6 +33,7 @@ class AndroidDatabase(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
 
     val bookNamesDao = BookNamesDao(this)
     val metadataDao = MetadataDao(this)
+    val readingProgressDao = ReadingProgressDao(this)
     val translationDao = TranslationDao(this)
     val translationInfoDao = TranslationInfoDao(this)
 
@@ -44,6 +42,7 @@ class AndroidDatabase(context: Context) : SQLiteOpenHelper(context, DATABASE_NAM
         try {
             BookNamesDao.createTable(db)
             MetadataDao.createTable(db)
+            ReadingProgressDao.createTable(db)
             TranslationInfoDao.createTable(db)
 
             db.setTransactionSuccessful()
@@ -122,6 +121,8 @@ class MetadataDao(private val sqliteHelper: SQLiteOpenHelper) {
         const val KEY_CURRENT_BOOK_INDEX = "currentBookIndex"
         const val KEY_CURRENT_CHAPTER_INDEX = "currentChapterIndex"
         const val KEY_CURRENT_VERSE_INDEX = "currentVerseIndex"
+        const val KEY_CONTINUOUS_READING_DAYS = "continuousReadingDays"
+        const val COLUMN_LAST_READING_TIMESTAMP = "lastReadingTimestamp"
 
         @WorkerThread
         fun createTable(db: SQLiteDatabase) {
@@ -186,6 +187,58 @@ class MetadataDao(private val sqliteHelper: SQLiteOpenHelper) {
             db.setTransactionSuccessful()
         } finally {
             db.endTransaction()
+        }
+    }
+}
+
+class ReadingProgressDao(private val sqliteHelper: SQLiteOpenHelper) {
+    companion object {
+        private const val TABLE_READING_PROGRESS = "readingProgress"
+        private const val COLUMN_BOOK_INDEX = "bookIndex"
+        private const val COLUMN_CHAPTER_INDEX = "chapterIndex"
+        private const val COLUMN_READ_COUNT = "readCount"
+        private const val COLUMN_LAST_READING_TIMESTAMP = "lastReadingTimestamp"
+
+        @WorkerThread
+        fun createTable(db: SQLiteDatabase) {
+            db.execSQL("CREATE TABLE $TABLE_READING_PROGRESS (" +
+                    "$COLUMN_BOOK_INDEX INTEGER NOT NULL, $COLUMN_CHAPTER_INDEX INTEGER NOT NULL, " +
+                    "$COLUMN_LAST_READING_TIMESTAMP INTEGER NOT NULL, $COLUMN_READ_COUNT INTEGER NOT NULL, " +
+                    "PRIMARY KEY ($COLUMN_BOOK_INDEX, $COLUMN_CHAPTER_INDEX));")
+        }
+    }
+
+    private val db by lazy { sqliteHelper.writableDatabase }
+
+    @WorkerThread
+    fun save(chapterReadingStatus: ReadingProgress.ChapterReadingStatus) {
+        val values = ContentValues(4)
+        values.put(COLUMN_BOOK_INDEX, chapterReadingStatus.bookIndex)
+        values.put(COLUMN_CHAPTER_INDEX, chapterReadingStatus.chapterIndex)
+        values.put(COLUMN_READ_COUNT, chapterReadingStatus.readCount)
+        values.put(COLUMN_LAST_READING_TIMESTAMP, chapterReadingStatus.lastReadingTimestamp)
+        db.insertWithOnConflict(TABLE_READING_PROGRESS, null, values, SQLiteDatabase.CONFLICT_REPLACE)
+    }
+
+    @WorkerThread
+    fun read(bookIndex: Int, chapterIndex: Int): ReadingProgress.ChapterReadingStatus {
+        var cursor: Cursor? = null
+        try {
+            cursor = db.query(TABLE_READING_PROGRESS, arrayOf(COLUMN_READ_COUNT, COLUMN_LAST_READING_TIMESTAMP),
+                    "$COLUMN_BOOK_INDEX = ? AND $COLUMN_CHAPTER_INDEX = ?",
+                    arrayOf(bookIndex.toString(), chapterIndex.toString()), null, null, null)
+            val readCount: Int
+            val lastReadingTimestamp: Long
+            if (cursor.count > 0 && cursor.moveToNext()) {
+                readCount = cursor.getInt(cursor.getColumnIndex(COLUMN_READ_COUNT))
+                lastReadingTimestamp = cursor.getLong(cursor.getColumnIndex(COLUMN_LAST_READING_TIMESTAMP))
+            } else {
+                readCount = 0
+                lastReadingTimestamp = 0L
+            }
+            return ReadingProgress.ChapterReadingStatus(bookIndex, chapterIndex, readCount, lastReadingTimestamp)
+        } finally {
+            cursor?.close()
         }
     }
 }
