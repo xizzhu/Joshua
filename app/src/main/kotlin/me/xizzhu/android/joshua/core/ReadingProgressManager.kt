9ace56d4d3ef90b428c84eead920e991a5b23dff
Xizhi Zhu <xizhi.zhu@gmail.com>
2019-03-15 22:23:31 -0700

    Added ReadingProgressManager to track reading progress

--- app/src/main/kotlin/me/xizzhu/android/joshua/core/ReadingProgressManager.kt
+++ app/src/main/kotlin/me/xizzhu/android/joshua/core/ReadingProgressManager.kt
@@ -16,8 +16,36 @@
 
 package me.xizzhu.android.joshua.core
 
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.channels.filter
+import me.xizzhu.android.joshua.core.repository.ReadingProgressRepository
+
 data class ReadingProgress(val continuousReadingDays: Int, val lastReadingTimestamp: Long,
                            val chapterReadingStatus: List<ChapterReadingStatus>) {
     data class ChapterReadingStatus(val bookIndex: Int, val chapterIndex: Int,
                                     val readCount: Int, val lastReadingTimestamp: Long)
+}
+
+class ReadingProgressManager(private val bibleReadingManager: BibleReadingManager,
+                             private val readingProgressRepository: ReadingProgressRepository) {
+    private var currentVerseIndexObserver: ReceiveChannel<VerseIndex>? = null
+
+    suspend fun startTracking() {
+        if (currentVerseIndexObserver != null) {
+            return
+        }
+
+        currentVerseIndexObserver = bibleReadingManager.observeCurrentVerseIndex()
+        currentVerseIndexObserver!!.filter { it.isValid() }
+                .consumeEach {
+                    readingProgressRepository.trackReadingProgress(
+                            it.bookIndex, it.chapterIndex, System.currentTimeMillis())
+                }
+    }
+
+    fun stopTracking() {
+        currentVerseIndexObserver?.cancel()
+        currentVerseIndexObserver = null
+    }
 }
---
+++ app/src/test/kotlin/me/xizzhu/android/joshua/core/ReadingProgressManagerTest.kt
@@ -0,0 +1,80 @@
+/*
+ * Copyright (C) 2019 Xizhi Zhu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package me.xizzhu.android.joshua.core
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.BroadcastChannel
+import kotlinx.coroutines.channels.ConflatedBroadcastChannel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import me.xizzhu.android.joshua.core.repository.ReadingProgressRepository
+import me.xizzhu.android.joshua.tests.BaseUnitTest
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers
+import org.mockito.Mock
+import org.mockito.Mockito.*
+
+class ReadingProgressManagerTest : BaseUnitTest() {
+    @Mock
+    private lateinit var bibleReadingManager: BibleReadingManager
+    @Mock
+    private lateinit var readingProgressRepository: ReadingProgressRepository
+    private lateinit var readingProgressManager: ReadingProgressManager
+
+    @Before
+    override fun setup() {
+        super.setup()
+        readingProgressManager = ReadingProgressManager(bibleReadingManager, readingProgressRepository)
+    }
+
+    @Test
+    fun testTracking() {
+        runBlocking {
+            val verseIndexChannel: BroadcastChannel<VerseIndex> = ConflatedBroadcastChannel(VerseIndex.INVALID)
+            `when`(bibleReadingManager.observeCurrentVerseIndex()).thenReturn(verseIndexChannel.openSubscription())
+
+            launch(Dispatchers.Unconfined) {
+                readingProgressManager.startTracking()
+            }
+            verify(readingProgressRepository, never()).trackReadingProgress(anyInt(), anyInt(), anyLong())
+
+            verseIndexChannel.send(VerseIndex(1, 2, 3))
+            verify(readingProgressRepository, times(1)).trackReadingProgress(anyInt(), anyInt(), anyLong())
+
+            readingProgressManager.stopTracking()
+        }
+    }
+
+    @Test
+    fun testStartTrackingMultipleTimes() {
+        runBlocking {
+            val verseIndexChannel: BroadcastChannel<VerseIndex> = ConflatedBroadcastChannel(VerseIndex.INVALID)
+            `when`(bibleReadingManager.observeCurrentVerseIndex()).thenReturn(verseIndexChannel.openSubscription())
+
+            launch(Dispatchers.Unconfined) {
+                readingProgressManager.startTracking()
+            }
+            launch(Dispatchers.Unconfined) {
+                readingProgressManager.startTracking()
+            }
+            verify(bibleReadingManager, times(1)).observeCurrentVerseIndex()
+
+            readingProgressManager.stopTracking()
+        }
+    }
+}
