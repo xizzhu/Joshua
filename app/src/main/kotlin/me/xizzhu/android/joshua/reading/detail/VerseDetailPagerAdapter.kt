6f6b47cf71cc8f7c9d0e9c971987e4e42881cdb2
Xizhi Zhu <xizhi.zhu@gmail.com>
2019-04-06 00:13:28 -0700

    Added basic UI for note in verse detail

--- app/src/main/kotlin/me/xizzhu/android/joshua/reading/detail/VerseDetailPagerAdapter.kt
+++ app/src/main/kotlin/me/xizzhu/android/joshua/reading/detail/VerseDetailPagerAdapter.kt
@@ -31,6 +31,8 @@ import me.xizzhu.android.joshua.ui.getBodyTextSize
 class VerseDetailPagerAdapter(context: Context) : PagerAdapter() {
     companion object {
         private const val PAGE_VERSES = 0
+        private const val PAGE_NOTE = 1
+        private const val PAGE_COUNT = 2
     }
 
     private val resources: Resources = context.resources
@@ -39,23 +41,42 @@ class VerseDetailPagerAdapter(context: Context) : PagerAdapter() {
     private var settings: Settings? = null
     private var verseDetail: VerseDetail? = null
 
-    override fun getCount(): Int {
-        return if (verseDetail != null && settings != null) 1 else 0
+    fun setSettings(settings: Settings) {
+        this.settings = settings
+        notifyDataSetChanged()
     }
 
-    override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val view = inflater.inflate(R.layout.page_verse_detail, container, false)
-        with(view.findViewById(R.id.detail) as TextView) {
-            setTextSize(TypedValue.COMPLEX_UNIT_PX,
-                    settings!!.getBodyTextSize(this@VerseDetailPagerAdapter.resources).toFloat())
-            text = when (position) {
-                PAGE_VERSES -> verseDetail!!.getTextForDisplay()
-                else -> ""
-            }
-        }
+    fun setVerse(verseDetail: VerseDetail) {
+        this.verseDetail = verseDetail
+        notifyDataSetChanged()
+    }
 
+    override fun getCount(): Int = if (settings != null && verseDetail != null) PAGE_COUNT else 0
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val view = when (position) {
+            PAGE_VERSES -> createVerseDetailView(container)
+            PAGE_NOTE -> createNoteView(container)
+            else -> throw IllegalArgumentException("Unsupported position: $position")
+        }
         container.addView(view)
         return view
+    }
+
+    private fun createVerseDetailView(container: ViewGroup): View {
+        return inflater.inflate(R.layout.page_verse_detail_verses, container, false).apply {
+            with(findViewById<TextView>(R.id.detail)) {
+                setTextSize(TypedValue.COMPLEX_UNIT_PX,
+                        settings!!.getBodyTextSize(this@VerseDetailPagerAdapter.resources).toFloat())
+                text = verseDetail!!.getTextForDisplay()
+            }
+        }
+    }
+
+    private fun createNoteView(container: ViewGroup): View {
+        return inflater.inflate(R.layout.page_verse_detail_note, container, false).apply {
+            // TODO
+        }
     }
 
     override fun destroyItem(container: ViewGroup, position: Int, obj: Any) {
@@ -69,19 +90,10 @@ class VerseDetailPagerAdapter(context: Context) : PagerAdapter() {
     override fun getPageTitle(position: Int): CharSequence {
         return when (position) {
             PAGE_VERSES -> resources.getString(R.string.text_verse_comparison)
+            PAGE_NOTE -> resources.getString(R.string.text_note)
             else -> ""
         }
     }
 
     override fun getItemPosition(obj: Any): Int = POSITION_NONE
-
-    fun setSettings(settings: Settings) {
-        this.settings = settings
-        notifyDataSetChanged()
-    }
-
-    fun setVerse(verseDetail: VerseDetail) {
-        this.verseDetail = verseDetail
-        notifyDataSetChanged()
-    }
 }
