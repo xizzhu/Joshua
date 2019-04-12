a564c8cbc138e48937b45cad279ec5d297d1eb6c
Xizhi Zhu <xizhi.zhu@gmail.com>
2019-04-12 10:37:34 -0700

    Bumped dependencies

--- buildSrc/src/main/kotlin/Configurations.kt
+++ buildSrc/src/main/kotlin/Configurations.kt
@@ -21,7 +21,7 @@ object Versions {
     const val coverallsGradle = "2.8.2"
 
     val java = JavaVersion.VERSION_1_8
-    const val kotlin = "1.3.21"
+    const val kotlin = "1.3.30"
     const val kotlinCoroutines = "1.1.1"
 
     const val compileSdk = 28
@@ -45,7 +45,7 @@ object Versions {
     const val dagger = "2.22.1"
 
     const val retrofit = "2.5.0"
-    const val okhttp = "3.14.0"
+    const val okhttp = "3.14.1"
     const val moshi = "1.8.0"
     const val okio = "2.2.2"
 
@@ -53,7 +53,7 @@ object Versions {
 
     const val testRunner = "1.1.0"
     const val testRules = "1.1.1"
-    const val mockito = "2.25.1"
+    const val mockito = "2.27.0"
 }
 
 object Dependencies {
