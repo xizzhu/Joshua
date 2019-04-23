5ed9a46eed70f8bf64a49d893b610983909c7e37
Xizhi Zhu <xizhi.zhu@gmail.com>
2019-04-22 23:26:25 -0700

    Logged to Crashlytics for INFO and above

--- app/src/main/kotlin/me/xizzhu/android/joshua/core/logger/android/CrashlyticsLogger.kt
+++ app/src/main/kotlin/me/xizzhu/android/joshua/core/logger/android/CrashlyticsLogger.kt
@@ -24,7 +24,9 @@ class CrashlyticsLogger : Logger {
     private val crashlyticsCore = Crashlytics.getInstance().core
 
     override fun log(@Log.Level level: Int, tag: String, msg: String) {
-        // do nothing
+        if (level > Log.INFO) {
+            crashlyticsCore.log(level, tag, msg)
+        }
     }
 
     override fun log(@Log.Level level: Int, tag: String, e: Throwable, msg: String) {
