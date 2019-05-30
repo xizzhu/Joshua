0d542ce34a76ee5d280e90d3be22242fd0979763
Xizhi Zhu <xizhi.zhu@gmail.com>
2019-05-29 23:16:57 -0700

    Simplified tests

--- app/src/test/kotlin/me/xizzhu/android/joshua/core/BibleReadingManagerTest.kt
+++ app/src/test/kotlin/me/xizzhu/android/joshua/core/BibleReadingManagerTest.kt
@@ -16,7 +16,6 @@
 
 package me.xizzhu.android.joshua.core
 
-import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.channels.first
 import kotlinx.coroutines.runBlocking
 import me.xizzhu.android.joshua.core.repository.BibleReadingRepository
@@ -49,12 +48,7 @@ class BibleReadingManagerTest : BaseUnitTest() {
     @Test
     fun testObserveCurrentVerseIndex() {
         runBlocking {
-            val observer = bibleReadingManager.observeCurrentVerseIndex()
-            observer.consumeEach {
-                if (it == VerseIndex(1, 2, 3)) {
-                    observer.cancel()
-                }
-            }
+            assertEquals(VerseIndex(1, 2, 3), bibleReadingManager.observeCurrentVerseIndex().first())
         }
     }
 
@@ -62,12 +56,8 @@ class BibleReadingManagerTest : BaseUnitTest() {
     fun testCurrentVerseIndex() {
         runBlocking {
             bibleReadingManager.saveCurrentVerseIndex(VerseIndex(4, 5, 6))
-            val observer = bibleReadingManager.observeCurrentVerseIndex()
-            observer.consumeEach {
-                if (it == VerseIndex(4, 5, 6)) {
-                    observer.cancel()
-                }
-            }
+
+            assertEquals(VerseIndex(4, 5, 6), bibleReadingManager.observeCurrentVerseIndex().first())
         }
     }
 
@@ -82,12 +72,7 @@ class BibleReadingManagerTest : BaseUnitTest() {
     @Test
     fun testObserveCurrentTranslation() {
         runBlocking {
-            val observer = bibleReadingManager.observeCurrentTranslation()
-            observer.consumeEach {
-                if (it == MockContents.kjvShortName) {
-                    observer.cancel()
-                }
-            }
+            assertEquals(MockContents.kjvShortName, bibleReadingManager.observeCurrentTranslation().first())
         }
     }
 
@@ -103,12 +88,7 @@ class BibleReadingManagerTest : BaseUnitTest() {
     fun testCurrentTranslation() {
         runBlocking {
             bibleReadingManager.saveCurrentTranslation(MockContents.cuvShortName)
-            val observer = bibleReadingManager.observeCurrentTranslation()
-            observer.consumeEach {
-                if (it == MockContents.cuvShortName) {
-                    observer.cancel()
-                }
-            }
+            assertEquals(MockContents.cuvShortName, bibleReadingManager.observeCurrentTranslation().first())
         }
     }
 
