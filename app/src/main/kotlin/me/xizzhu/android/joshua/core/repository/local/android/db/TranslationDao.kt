4b5abbe4ca96cbc0c148c31af412faf9657ca680
Xizhi Zhu <xizhi.zhu@gmail.com>
2019-11-27 23:50:56 -0800

    Used ASK to access SQLite in TranslationDao

--- app/src/main/kotlin/me/xizzhu/android/joshua/core/repository/local/android/db/TranslationDao.kt
+++ app/src/main/kotlin/me/xizzhu/android/joshua/core/repository/local/android/db/TranslationDao.kt
@@ -16,15 +16,13 @@
 
 package me.xizzhu.android.joshua.core.repository.local.android.db
 
-import android.content.ContentValues
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import androidx.annotation.WorkerThread
-import me.xizzhu.android.ask.db.withTransaction
+import me.xizzhu.android.ask.db.*
 import me.xizzhu.android.joshua.core.Verse
 import me.xizzhu.android.joshua.core.VerseIndex
 import me.xizzhu.android.logger.Log
-import java.lang.StringBuilder
 import kotlin.math.max
 
 class TranslationDao(sqliteHelper: SQLiteOpenHelper) {
@@ -41,40 +39,28 @@ class TranslationDao(sqliteHelper: SQLiteOpenHelper) {
 
     @WorkerThread
     fun read(translationShortName: String, bookIndex: Int, chapterIndex: Int): List<Verse> = db.withTransaction {
-        if (!hasTable(translationShortName)) return emptyList()
+        if (!hasTableAndLogIfNoTable(translationShortName)) return@withTransaction emptyList()
 
-        query(translationShortName, arrayOf(COLUMN_TEXT),
-                "$COLUMN_BOOK_INDEX = ? AND $COLUMN_CHAPTER_INDEX = ?", arrayOf(bookIndex.toString(), chapterIndex.toString()),
-                null, null, "$COLUMN_VERSE_INDEX ASC").use {
-            val verses = ArrayList<Verse>(it.count)
-            var verseIndex = 0
-            while (it.moveToNext()) {
-                verses.add(Verse(VerseIndex(bookIndex, chapterIndex, verseIndex++),
-                        Verse.Text(translationShortName, it.getString(0)), emptyList()))
-            }
-            return verses
+        select(translationShortName, COLUMN_TEXT) {
+            (COLUMN_BOOK_INDEX eq bookIndex) and (COLUMN_CHAPTER_INDEX eq chapterIndex)
+        }.orderBy(COLUMN_VERSE_INDEX).toListIndexed { index, row ->
+            Verse(VerseIndex(bookIndex, chapterIndex, index),
+                    Verse.Text(translationShortName, row.getString(COLUMN_TEXT)), emptyList())
         }
     }
 
     @WorkerThread
-    private fun SQLiteDatabase.hasTable(name: String, logIfNoTable: Boolean = true): Boolean {
-        rawQuery("SELECT DISTINCT tbl_name FROM sqlite_master WHERE tbl_name = '$name'", null).use {
-            val hasTable = it.count > 0
-            if (logIfNoTable && !hasTable) {
-                Log.e(TAG, "", IllegalStateException("Missing translation $name"))
-            }
-            return hasTable
-        }
-    }
+    private fun SQLiteDatabase.hasTableAndLogIfNoTable(table: String): Boolean = hasTable(table)
+            .also { hasTable -> if (!hasTable) Log.e(TAG, "", IllegalStateException("Missing translation $table")) }
 
     @WorkerThread
     fun read(translationShortName: String, parallelTranslations: List<String>,
              bookIndex: Int, chapterIndex: Int): List<Verse> = db.withTransaction {
-        if (!hasTable(translationShortName)) return emptyList()
+        if (!hasTableAndLogIfNoTable(translationShortName)) return emptyList()
 
         val primaryTexts = readVerseTexts(translationShortName, bookIndex, chapterIndex)
         var versesCount = primaryTexts.size
-        val parallelTexts = ArrayList<ArrayList<Verse.Text>>(parallelTranslations.size).apply {
+        val parallelTexts = ArrayList<List<Verse.Text>>(parallelTranslations.size).apply {
             val countVerses: (List<Verse.Text>) -> Unit = { versesCount = max(versesCount, it.size) }
             parallelTranslations.forEach { add(readVerseTexts(it, bookIndex, chapterIndex).also(countVerses)) }
         }
@@ -98,104 +84,66 @@ class TranslationDao(sqliteHelper: SQLiteOpenHelper) {
 
     @WorkerThread
     private fun SQLiteDatabase.readVerseTexts(translation: String,
-                                              bookIndex: Int, chapterIndex: Int): ArrayList<Verse.Text> {
-        if (!hasTable(translation)) return ArrayList()
-
-        query(translation, arrayOf(COLUMN_TEXT),
-                "$COLUMN_BOOK_INDEX = ? AND $COLUMN_CHAPTER_INDEX = ?",
-                arrayOf(bookIndex.toString(), chapterIndex.toString()),
-                null, null, "$COLUMN_VERSE_INDEX ASC").use {
-            val texts = ArrayList<Verse.Text>(it.count)
-            while (it.moveToNext()) {
-                texts.add(Verse.Text(translation, it.getString(0)))
+                                              bookIndex: Int, chapterIndex: Int): List<Verse.Text> =
+            if (hasTableAndLogIfNoTable(translation)) {
+                select(translation, COLUMN_TEXT) {
+                    (COLUMN_BOOK_INDEX eq bookIndex) and (COLUMN_CHAPTER_INDEX eq chapterIndex)
+                }.orderBy(COLUMN_VERSE_INDEX).toList { Verse.Text(translation, it.getString(COLUMN_TEXT)) }
+            } else {
+                emptyList()
             }
-            return texts
-        }
+
+    @WorkerThread
+    fun read(translationShortName: String, verseIndex: VerseIndex): Verse = db.withTransaction {
+        if (!hasTableAndLogIfNoTable(translationShortName)) return@withTransaction Verse.INVALID
+
+        select(translationShortName, COLUMN_TEXT) {
+            (COLUMN_BOOK_INDEX eq verseIndex.bookIndex) and (COLUMN_CHAPTER_INDEX eq verseIndex.chapterIndex) and (COLUMN_VERSE_INDEX eq verseIndex.verseIndex)
+        }.firstOrDefault(Verse.INVALID) { Verse(verseIndex, Verse.Text(translationShortName, it.getString(COLUMN_TEXT)), emptyList()) }
     }
 
     @WorkerThread
-    fun read(translationShortName: String, verseIndex: VerseIndex): Verse {
-        if (!verseIndex.isValid()) return Verse.INVALID
+    fun search(translationShortName: String, query: String): List<Verse> = db.withTransaction {
+        if (query.isBlank() || !hasTableAndLogIfNoTable(translationShortName)) return@withTransaction emptyList()
 
-        db.withTransaction {
-            if (!hasTable(translationShortName)) return Verse.INVALID
-
-            query(translationShortName, arrayOf(COLUMN_TEXT),
-                    "$COLUMN_BOOK_INDEX = ? AND $COLUMN_CHAPTER_INDEX = ? AND $COLUMN_VERSE_INDEX = ?",
-                    arrayOf(verseIndex.bookIndex.toString(), verseIndex.chapterIndex.toString(), verseIndex.verseIndex.toString()),
-                    null, null, null).use {
-                return if (it.moveToNext()) {
-                    Verse(verseIndex, Verse.Text(translationShortName, it.getString(0)), emptyList())
-                } else {
-                    Verse.INVALID
+        select(translationShortName) {
+            var condition: Condition = noOp()
+            query.trim().replace("\\s+", " ").split(" ").forEach { keyword ->
+                (COLUMN_TEXT like "%%$keyword%%").run {
+                    condition = if (condition == Condition.NoOp) this else condition and this
                 }
             }
-        }
-    }
-
-    @WorkerThread
-    fun search(translationShortName: String, query: String): List<Verse> {
-        val keywords = query.trim().let {
-            if (it.isEmpty()) return emptyList()
-            return@let it.replace("\\s+", " ").split(" ")
-        }
-
-        val singleSelection = "$COLUMN_TEXT LIKE ?"
-        val selection = StringBuilder()
-        val selectionArgs = Array(keywords.size) { "" }
-        for (i in keywords.indices) {
-            if (selection.isNotEmpty()) {
-                selection.append(" AND ")
-            }
-            selection.append(singleSelection)
-
-            selectionArgs[i] = "%%${keywords[i]}%%"
-        }
-
-        db.withTransaction {
-            if (!hasTable(translationShortName)) return emptyList()
-
-            query(translationShortName,
-                    arrayOf(COLUMN_BOOK_INDEX, COLUMN_CHAPTER_INDEX, COLUMN_VERSE_INDEX, COLUMN_TEXT),
-                    selection.toString(), selectionArgs, null, null,
-                    "$COLUMN_BOOK_INDEX ASC, $COLUMN_CHAPTER_INDEX ASC, $COLUMN_VERSE_INDEX ASC").use {
-                val verses = ArrayList<Verse>(it.count)
-                if (it.count > 0) {
-                    val bookColumnIndex = it.getColumnIndex(COLUMN_BOOK_INDEX)
-                    val chapterColumnIndex = it.getColumnIndex(COLUMN_CHAPTER_INDEX)
-                    val verseColumnIndex = it.getColumnIndex(COLUMN_VERSE_INDEX)
-                    val textColumnIndex = it.getColumnIndex(COLUMN_TEXT)
-                    while (it.moveToNext()) {
-                        val verseIndex = VerseIndex(it.getInt(bookColumnIndex),
-                                it.getInt(chapterColumnIndex), it.getInt(verseColumnIndex))
-                        verses.add(Verse(verseIndex, Verse.Text(translationShortName, it.getString(textColumnIndex)), emptyList()))
-                    }
-                }
-                return verses
-            }
+            condition
+        }.toList { row ->
+            Verse(
+                    VerseIndex(row.getInt(COLUMN_BOOK_INDEX), row.getInt(COLUMN_CHAPTER_INDEX), row.getInt(COLUMN_VERSE_INDEX)),
+                    Verse.Text(translationShortName, row.getString(COLUMN_TEXT)), emptyList()
+            )
         }
     }
 
     @WorkerThread
     fun save(translationShortName: String, verses: Map<Pair<Int, Int>, List<String>>) = db.withTransaction {
-        if (hasTable(translationShortName, false)) {
+        if (hasTable(translationShortName)) {
             Log.e(TAG, "", IllegalStateException("Translation $translationShortName already installed"))
             remove(translationShortName)
         }
-        execSQL("CREATE TABLE IF NOT EXISTS $translationShortName (" +
-                "$COLUMN_BOOK_INDEX INTEGER NOT NULL, $COLUMN_CHAPTER_INDEX INTEGER NOT NULL, " +
-                "$COLUMN_VERSE_INDEX INTEGER NOT NULL, $COLUMN_TEXT TEXT NOT NULL, " +
-                "PRIMARY KEY($COLUMN_BOOK_INDEX, $COLUMN_CHAPTER_INDEX, $COLUMN_VERSE_INDEX));")
+        createTable(translationShortName) {
+            it[COLUMN_BOOK_INDEX] = INTEGER + PRIMARY_KEY + NOT_NULL
+            it[COLUMN_CHAPTER_INDEX] = INTEGER + PRIMARY_KEY + NOT_NULL
+            it[COLUMN_VERSE_INDEX] = INTEGER + PRIMARY_KEY + NOT_NULL
+            it[COLUMN_TEXT] = TEXT + NOT_NULL
+        }
 
-        val values = ContentValues(4)
-        for (entry in verses) {
-            with(values) {
-                put(COLUMN_BOOK_INDEX, entry.key.first)
-                put(COLUMN_CHAPTER_INDEX, entry.key.second)
-                for ((verseIndex, verse) in entry.value.withIndex()) {
-                    put(COLUMN_VERSE_INDEX, verseIndex)
-                    put(COLUMN_TEXT, verse)
-                    insertWithOnConflict(translationShortName, null, this, SQLiteDatabase.CONFLICT_REPLACE)
+        verses.forEach { (index, verseList) ->
+            val bookIndex = index.first
+            val chapterIndex = index.second
+            verseList.forEachIndexed { verseIndex, verse ->
+                insert(translationShortName, SQLiteDatabase.CONFLICT_REPLACE) {
+                    it[COLUMN_BOOK_INDEX] = bookIndex
+                    it[COLUMN_CHAPTER_INDEX] = chapterIndex
+                    it[COLUMN_VERSE_INDEX] = verseIndex
+                    it[COLUMN_TEXT] = verse
                 }
             }
         }
@@ -203,6 +151,6 @@ class TranslationDao(sqliteHelper: SQLiteOpenHelper) {
 
     @WorkerThread
     fun remove(translationShortName: String) {
-        db.execSQL("DROP TABLE IF EXISTS $translationShortName")
+        db.dropTable(translationShortName)
     }
 }
