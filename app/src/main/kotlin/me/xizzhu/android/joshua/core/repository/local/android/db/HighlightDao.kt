34c24d44520fe2e5752105166297e323350ca4dd
Xizhi Zhu <xizhi.zhu@gmail.com>
2019-11-29 21:39:54 -0800

    Added a VerseAnnotationDao for common logics

--- app/src/main/kotlin/me/xizzhu/android/joshua/core/repository/local/android/db/HighlightDao.kt
+++ app/src/main/kotlin/me/xizzhu/android/joshua/core/repository/local/android/db/HighlightDao.kt
@@ -16,93 +16,29 @@
 
 package me.xizzhu.android.joshua.core.repository.local.android.db
 
-import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import androidx.annotation.WorkerThread
 import me.xizzhu.android.ask.db.*
-import me.xizzhu.android.joshua.core.Constants
 import me.xizzhu.android.joshua.core.Highlight
 import me.xizzhu.android.joshua.core.VerseIndex
 
-class HighlightDao(sqliteHelper: SQLiteOpenHelper) {
+class HighlightDao(sqliteHelper: SQLiteOpenHelper) : VerseAnnotationDao<Highlight>(sqliteHelper, TABLE_HIGHLIGHT) {
     companion object {
         private const val TABLE_HIGHLIGHT = "highlight"
-        private const val COLUMN_BOOK_INDEX = "bookIndex"
-        private const val COLUMN_CHAPTER_INDEX = "chapterIndex"
-        private const val COLUMN_VERSE_INDEX = "verseIndex"
         private const val COLUMN_COLOR = "color"
-        private const val COLUMN_TIMESTAMP = "timestamp"
     }
 
-    private val db by lazy { sqliteHelper.writableDatabase }
-
-    @WorkerThread
-    fun createTable(db: SQLiteDatabase) {
-        db.createTable(TABLE_HIGHLIGHT) {
-            it[COLUMN_BOOK_INDEX] = INTEGER + PRIMARY_KEY + NOT_NULL
-            it[COLUMN_CHAPTER_INDEX] = INTEGER + PRIMARY_KEY + NOT_NULL
-            it[COLUMN_VERSE_INDEX] = INTEGER + PRIMARY_KEY + NOT_NULL
-            it[COLUMN_COLOR] = INTEGER + NOT_NULL
-            it[COLUMN_TIMESTAMP] = INTEGER + NOT_NULL
-        }
+    override fun MutableMap<String, ColumnModifiers>.putCustomColumnModifiers() {
+        put(COLUMN_COLOR, INTEGER + NOT_NULL)
     }
 
-    fun read(@Constants.SortOrder sortOrder: Int): List<Highlight> = db.select(TABLE_HIGHLIGHT)
-            .apply {
-                when (sortOrder) {
-                    Constants.SORT_BY_DATE -> orderBy(COLUMN_TIMESTAMP, sortOrder = SortOrder.DESCENDING)
-                    Constants.SORT_BY_BOOK -> orderBy(COLUMN_BOOK_INDEX, COLUMN_CHAPTER_INDEX, COLUMN_VERSE_INDEX)
-                    else -> throw IllegalArgumentException("Unsupported sort order - $sortOrder")
-                }
-            }.toList { row ->
-                Highlight(
-                        VerseIndex(
-                                row.getInt(COLUMN_BOOK_INDEX),
-                                row.getInt(COLUMN_CHAPTER_INDEX),
-                                row.getInt(COLUMN_VERSE_INDEX)
-                        ),
-                        row.getInt(COLUMN_COLOR),
-                        row.getLong(COLUMN_TIMESTAMP)
-                )
-            }
+    override fun Map<String, Any?>.toVerseAnnotation(verseIndex: VerseIndex, timestamp: Long): Highlight =
+            Highlight(verseIndex, getInt(COLUMN_COLOR), timestamp)
 
-    fun read(bookIndex: Int, chapterIndex: Int): List<Highlight> =
-            db.select(TABLE_HIGHLIGHT, COLUMN_VERSE_INDEX, COLUMN_COLOR, COLUMN_TIMESTAMP) {
-                (COLUMN_BOOK_INDEX eq bookIndex) and (COLUMN_CHAPTER_INDEX eq chapterIndex)
-            }
-                    .orderBy(COLUMN_VERSE_INDEX)
-                    .toList { row ->
-                        Highlight(
-                                VerseIndex(bookIndex, chapterIndex, row.getInt(COLUMN_VERSE_INDEX)),
-                                row.getInt(COLUMN_COLOR),
-                                row.getLong(COLUMN_TIMESTAMP)
-                        )
-                    }
+    override fun getCustomColumns(): Array<String> = arrayOf(COLUMN_COLOR)
 
-    fun read(verseIndex: VerseIndex): Highlight =
-            db.select(TABLE_HIGHLIGHT, COLUMN_COLOR, COLUMN_TIMESTAMP) {
-                (COLUMN_BOOK_INDEX eq verseIndex.bookIndex) and (COLUMN_CHAPTER_INDEX eq verseIndex.chapterIndex) and (COLUMN_VERSE_INDEX eq verseIndex.verseIndex)
-            }.firstOrDefault({ Highlight(verseIndex, Highlight.COLOR_NONE, -1L) }) { row ->
-                Highlight(verseIndex, row.getInt(COLUMN_COLOR), row.getLong(COLUMN_TIMESTAMP))
-            }
+    override fun defaultVerseAnnotation(verseIndex: VerseIndex): Highlight = Highlight(verseIndex, Highlight.COLOR_NONE, -1L)
 
-    fun save(highlight: Highlight) {
-        db.insert(TABLE_HIGHLIGHT, SQLiteDatabase.CONFLICT_REPLACE) {
-            it[COLUMN_BOOK_INDEX] = highlight.verseIndex.bookIndex
-            it[COLUMN_CHAPTER_INDEX] = highlight.verseIndex.chapterIndex
-            it[COLUMN_VERSE_INDEX] = highlight.verseIndex.verseIndex
-            it[COLUMN_COLOR] = highlight.color
-            it[COLUMN_TIMESTAMP] = highlight.timestamp
-        }
-    }
-
-    fun save(highlights: List<Highlight>) {
-        db.transaction { highlights.forEach { save(it) } }
-    }
-
-    fun remove(verseIndex: VerseIndex) {
-        db.delete(TABLE_HIGHLIGHT) {
-            (COLUMN_BOOK_INDEX eq verseIndex.bookIndex) and (COLUMN_CHAPTER_INDEX eq verseIndex.chapterIndex) and (COLUMN_VERSE_INDEX eq verseIndex.verseIndex)
-        }
+    override fun MutableMap<String, Any?>.putCustomColumnValues(verseAnnotation: Highlight) {
+        put(COLUMN_COLOR, verseAnnotation.color)
     }
 }
--- app/src/main/kotlin/me/xizzhu/android/joshua/core/repository/local/android/db/NoteDao.kt
+++ app/src/main/kotlin/me/xizzhu/android/joshua/core/repository/local/android/db/NoteDao.kt
@@ -16,91 +16,29 @@
 
 package me.xizzhu.android.joshua.core.repository.local.android.db
 
-import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import androidx.annotation.WorkerThread
 import me.xizzhu.android.ask.db.*
-import me.xizzhu.android.joshua.core.Constants
 import me.xizzhu.android.joshua.core.Note
 import me.xizzhu.android.joshua.core.VerseIndex
 
-class NoteDao(sqliteHelper: SQLiteOpenHelper) {
+class NoteDao(sqliteHelper: SQLiteOpenHelper) : VerseAnnotationDao<Note>(sqliteHelper, TABLE_NOTE) {
     companion object {
         private const val TABLE_NOTE = "note"
-        private const val COLUMN_BOOK_INDEX = "bookIndex"
-        private const val COLUMN_CHAPTER_INDEX = "chapterIndex"
-        private const val COLUMN_VERSE_INDEX = "verseIndex"
         private const val COLUMN_NOTE = "note"
-        private const val COLUMN_TIMESTAMP = "timestamp"
     }
 
-    private val db by lazy { sqliteHelper.writableDatabase }
-
-    @WorkerThread
-    fun createTable(db: SQLiteDatabase) {
-        db.createTable(TABLE_NOTE) {
-            it[COLUMN_BOOK_INDEX] = INTEGER + PRIMARY_KEY + NOT_NULL
-            it[COLUMN_CHAPTER_INDEX] = INTEGER + PRIMARY_KEY + NOT_NULL
-            it[COLUMN_VERSE_INDEX] = INTEGER + PRIMARY_KEY + NOT_NULL
-            it[COLUMN_NOTE] = TEXT + NOT_NULL
-            it[COLUMN_TIMESTAMP] = INTEGER + NOT_NULL
-        }
+    override fun MutableMap<String, ColumnModifiers>.putCustomColumnModifiers() {
+        put(COLUMN_NOTE, TEXT + NOT_NULL)
     }
 
-    fun read(@Constants.SortOrder sortOrder: Int): List<Note> = db.select(TABLE_NOTE)
-            .apply {
-                when (sortOrder) {
-                    Constants.SORT_BY_DATE -> orderBy(COLUMN_TIMESTAMP, sortOrder = SortOrder.DESCENDING)
-                    Constants.SORT_BY_BOOK -> orderBy(COLUMN_BOOK_INDEX, COLUMN_CHAPTER_INDEX, COLUMN_VERSE_INDEX)
-                    else -> throw IllegalArgumentException("Unsupported sort order - $sortOrder")
-                }
-            }.toList { row ->
-                Note(
-                        VerseIndex(
-                                row.getInt(COLUMN_BOOK_INDEX),
-                                row.getInt(COLUMN_CHAPTER_INDEX),
-                                row.getInt(COLUMN_VERSE_INDEX)
-                        ),
-                        row.getString(COLUMN_NOTE),
-                        row.getLong(COLUMN_TIMESTAMP)
-                )
-            }
+    override fun Map<String, Any?>.toVerseAnnotation(verseIndex: VerseIndex, timestamp: Long): Note =
+            Note(verseIndex, getString(COLUMN_NOTE), timestamp)
 
-    fun read(bookIndex: Int, chapterIndex: Int): List<Note> =
-            db.select(TABLE_NOTE, COLUMN_VERSE_INDEX, COLUMN_NOTE, COLUMN_TIMESTAMP) {
-                (COLUMN_BOOK_INDEX eq bookIndex) and (COLUMN_CHAPTER_INDEX eq chapterIndex)
-            }
-                    .orderBy(COLUMN_VERSE_INDEX)
-                    .toList { row ->
-                        Note(
-                                VerseIndex(bookIndex, chapterIndex, row.getInt(COLUMN_VERSE_INDEX)),
-                                row.getString(COLUMN_NOTE),
-                                row.getLong(COLUMN_TIMESTAMP)
-                        )
-                    }
+    override fun getCustomColumns(): Array<String> = arrayOf(COLUMN_NOTE)
 
-    fun read(verseIndex: VerseIndex): Note =
-            db.select(TABLE_NOTE, COLUMN_NOTE, COLUMN_TIMESTAMP) {
-                (COLUMN_BOOK_INDEX eq verseIndex.bookIndex) and (COLUMN_CHAPTER_INDEX eq verseIndex.chapterIndex) and (COLUMN_VERSE_INDEX eq verseIndex.verseIndex)
-            }.firstOrDefault({ Note(verseIndex, "", -1L) }) { Note(verseIndex, it.getString(COLUMN_NOTE), it.getLong(COLUMN_TIMESTAMP)) }
+    override fun defaultVerseAnnotation(verseIndex: VerseIndex): Note = Note(verseIndex, "", -1L)
 
-    fun save(note: Note) {
-        db.insert(TABLE_NOTE, SQLiteDatabase.CONFLICT_REPLACE) {
-            it[COLUMN_BOOK_INDEX] = note.verseIndex.bookIndex
-            it[COLUMN_CHAPTER_INDEX] = note.verseIndex.chapterIndex
-            it[COLUMN_VERSE_INDEX] = note.verseIndex.verseIndex
-            it[COLUMN_NOTE] = note.note
-            it[COLUMN_TIMESTAMP] = note.timestamp
-        }
-    }
-
-    fun save(notes: List<Note>) {
-        db.transaction { notes.forEach { save(it) } }
-    }
-
-    fun remove(verseIndex: VerseIndex) {
-        db.delete(TABLE_NOTE) {
-            (COLUMN_BOOK_INDEX eq verseIndex.bookIndex) and (COLUMN_CHAPTER_INDEX eq verseIndex.chapterIndex) and (COLUMN_VERSE_INDEX eq verseIndex.verseIndex)
-        }
+    override fun MutableMap<String, Any?>.putCustomColumnValues(verseAnnotation: Note) {
+        put(COLUMN_NOTE, verseAnnotation.note)
     }
 }
