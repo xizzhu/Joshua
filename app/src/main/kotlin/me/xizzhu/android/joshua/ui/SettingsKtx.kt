29c21c8c8f496814e18c76dcc766e97e95117aac
Xizhi Zhu <xizhi.zhu@gmail.com>
2020-01-07 23:51:00 -0800

    Reorganized SettingsKtx and ViewKtx

--- app/src/main/kotlin/me/xizzhu/android/joshua/ui/SettingsKtx.kt
+++ app/src/main/kotlin/me/xizzhu/android/joshua/ui/SettingsKtx.kt
@@ -16,43 +16,13 @@
 
 package me.xizzhu.android.joshua.ui
 
-import android.content.Context
 import android.content.res.Resources
-import android.content.res.TypedArray
 import android.graphics.Color
-import android.text.TextUtils
 import android.util.TypedValue
-import android.view.View
-import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import androidx.annotation.*
 import me.xizzhu.android.joshua.R
 import me.xizzhu.android.joshua.core.Settings
-
-fun TextView.setText(a: TypedArray, @StyleableRes index: Int) {
-    val resourceId = a.getResourceId(index, -1)
-    if (resourceId != -1) {
-        setText(resourceId)
-    } else {
-        val text = a.getString(index)
-        if (!TextUtils.isEmpty(text)) {
-            setText(text)
-        }
-    }
-}
-
-fun View.setBackground(resId: Int) {
-    val out = TypedValue()
-    context.theme.resolveAttribute(resId, out, true)
-    setBackgroundResource(out.resourceId)
-}
-
-fun View.hideKeyboard() {
-    if (hasFocus()) {
-        (context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
-                .hideSoftInputFromWindow(windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
-    }
-}
 
 @ColorInt
 fun Settings.getBackgroundColor(): Int = if (nightModeOn) Color.BLACK else Color.WHITE
--- app/src/main/kotlin/me/xizzhu/android/joshua/ui/ViewKtx.kt
+++ app/src/main/kotlin/me/xizzhu/android/joshua/ui/ViewKtx.kt
@@ -20,9 +20,15 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ArgbEvaluator
 import android.animation.ValueAnimator
+import android.content.Context
+import android.content.res.TypedArray
+import android.text.TextUtils
+import android.util.TypedValue
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import androidx.annotation.ColorInt
+import androidx.annotation.StyleableRes
 
 private const val ANIMATION_DURATION = 300L
 
@@ -51,6 +57,19 @@ fun View.fadeOut() {
             })
 }
 
+fun View.setBackground(resId: Int) {
+    val out = TypedValue()
+    context.theme.resolveAttribute(resId, out, true)
+    setBackgroundResource(out.resourceId)
+}
+
+fun View.hideKeyboard() {
+    if (hasFocus()) {
+        (context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                .hideSoftInputFromWindow(windowToken, InputMethodManager.HIDE_NOT_ALWAYS)
+    }
+}
+
 fun TextView.animateTextColor(@ColorInt to: Int) {
     val from = currentTextColor
     val argbEvaluator = ArgbEvaluator()
@@ -60,6 +79,18 @@ fun TextView.animateTextColor(@ColorInt to: Int) {
             val color = argbEvaluator.evaluate(fraction, from, to) as Int
             setTextColor(color)
         }
-        start()
+        setDuration(ANIMATION_DURATION).start()
+    }
+}
+
+fun TextView.setText(a: TypedArray, @StyleableRes index: Int) {
+    val resourceId = a.getResourceId(index, -1)
+    if (resourceId != -1) {
+        setText(resourceId)
+    } else {
+        val text = a.getString(index)
+        if (!TextUtils.isEmpty(text)) {
+            setText(text)
+        }
     }
 }
