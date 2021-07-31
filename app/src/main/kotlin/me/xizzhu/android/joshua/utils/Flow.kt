dc5717b6d5fd1e1c3541f6a2a926dfb3b5e21a5a
Xizhi Zhu <xizhi.zhu@gmail.com>
2021-07-31 13:28:28 -0500

    It's already 2021

--- app/src/main/kotlin/me/xizzhu/android/joshua/utils/Flow.kt
+++ app/src/main/kotlin/me/xizzhu/android/joshua/utils/Flow.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 Xizhi Zhu
+ * Copyright (C) 2021 Xizhi Zhu
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
