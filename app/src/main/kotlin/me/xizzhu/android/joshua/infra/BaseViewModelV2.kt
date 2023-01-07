bed8b139ae1798667347e93e8571e5fca0263ae9
Xizhi Zhu <xizhi.zhu@protonmail.com>
2023-01-07 09:47:40 +0900

    Remove unused code

--- app/src/main/kotlin/me/xizzhu/android/joshua/infra/BaseViewModelV2.kt
+++ app/src/main/kotlin/me/xizzhu/android/joshua/infra/BaseViewModelV2.kt
@@ -21,7 +21,6 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
